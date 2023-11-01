8aef4222e1c7945b5c05a9ca3cc3062956729e77
alberto8812 <cvelascosaavedra@gmail.com>
2023-11-01 15:50:12 -0500

    use onError and onmutate

--- src/products/hooks/useProductMutation.tsx
+++ src/products/hooks/useProductMutation.tsx
@@ -7,7 +7,24 @@ export const useProductMutation = () => {
 
     const mutation=useMutation({
     mutationFn:productaActions.createProduct,//llamamos la funcion que hace la peticion post
-    onSuccess:(product)=>{
+
+    onMutate:(data)=>{
+
+        //seiguiente paso es crear un producto optimista
+       const optimisticProduct={id:Math.random(),...data}//va a ser igual al producto creado , conu un id asignado
+        //los siguiente es afectar el query de la cache 
+        queryClient.setQueriesData<ProductosData[]>(
+            {queryKey:['products',{filterKey:data.category}]},
+            (old)=>{
+                if(!old) return [optimisticProduct]
+                return [...old,optimisticProduct]
+             }
+        );
+        return {optimisticProduct}
+        },
+
+    
+    onSuccess:(product,variables,context)=>{
       /**
        * evitar recarar el navegador cuando se crea un producto
        *  */  
@@ -21,13 +38,32 @@ export const useProductMutation = () => {
             {queryKey:['products',{filterKey:product.category}]},
             (old)=>{
                if(!old) return [product]
-               return [...old,product]
+               return  old.map(data=>data.id===context?.optimisticProduct.id? product:data)
             }
             
             
         )
 
     },
+
+    onError:(error,varibles,context)=>{
+        queryClient.removeQueries(
+            {
+                queryKey:["produdct",context?.optimisticProduct.id]
+            }
+        );
+        queryClient.setQueriesData<ProductosData[]>(
+            
+            {queryKey:['products',{filterKey:varibles.category}]},
+            (old)=>{
+               if(!old) return []
+               return  old.filter(data=>data.id!==context?.optimisticProduct.id)
+            }
+            
+            
+        )
+
+    }
 
   })
   
