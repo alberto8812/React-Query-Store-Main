66d038377b828ff857124f210778ebe7ace42391
alberto8812 <cvelascosaavedra@gmail.com>
2023-11-01 11:23:09 -0500

    implment watch  in react form

--- src/products/hooks/usePrefetchProducts.tsx
+++ src/products/hooks/usePrefetchProducts.tsx
@@ -3,6 +3,7 @@ import { productaActions } from '..';
 
 export const usePrefetchProducts = () => {
     const queryClient=useQueryClient();
+    
     const preFechProduct=async(id:number)=>{
       queryClient.prefetchQuery(
         {
